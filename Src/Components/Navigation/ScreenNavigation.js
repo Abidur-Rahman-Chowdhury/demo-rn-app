8ca711d27ef687bcadd4c4f64c74dd97bf1e6ee6
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2023-01-25 00:20:18 +0600

    fully complete first screen view also implement the plus button

--- Src/Components/Navigation/ScreenNavigation.js
+++ Src/Components/Navigation/ScreenNavigation.js
@@ -2,6 +2,7 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreens from '../../Screens/HomeScreens';
+import NextScreen from '../../Screens/NextScreen';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 const Stack = createNativeStackNavigator();
@@ -23,7 +24,7 @@ export default function ScreenNavigation() {
             </View>
           ),
           headerStyle: {
-            backgroundColor: 'red',
+            backgroundColor: '#850101',
           },
           headerRight: () => (
             <View>
@@ -36,6 +37,33 @@ export default function ScreenNavigation() {
       <Stack.Screen
         name="HomeScreenBottom"
         component={HomeScreens}></Stack.Screen>
+      <Stack.Screen
+        name="NextScreen"
+        component={NextScreen}
+        options={{
+          headerTitle: () => (
+            <View>
+              <View style={{flex: 1}}>
+                <Text
+                  style={{fontSize: 15, color: 'white'}}>
+                  কন্টাক্টস লিস্ট
+                </Text>
+              </View>
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: '#850101',
+          },
+          headerRight: () => (
+            <View>
+              <Icon
+                name="search"
+                style={{color: 'white', fontSize: 16, paddingRight: 15}}></Icon>
+            </View>
+          ),
+        }}
+      
+      />
     </Stack.Navigator>
   );
 }
--- Src/Screens/HomeScreens.js
+++ Src/Screens/HomeScreens.js
@@ -1,22 +1,112 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
 export default function HomeScreens(props) {
+  const navigation = useNavigation()
   return (
-    <View style={{ flex: 1 }}>
-        
-    <View>
-      <View>
-        <Text>মোট বকেয়া</Text>
-      </View>
-      <View>
-        <Text></Text>
+    <View style={{flex: 1, marginTop: 20, paddingLeft: 20, paddingRight: 20}}>
+      {/* first section */}
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomColor: 'gray',
+          borderBottomWidth: 1,
+          paddingBottom: 30,
+        }}>
+        <View style={{flex: 3}}>
+          <View>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: '600'}}>
+              মোট বকেয়া
+            </Text>
+            <Text style={{fontSize: 35, color: 'red'}}>০.০০</Text>
+          </View>
+          <View>
+            <Text style={{fontSize: 20, color: 'black', fontWeight: '600'}}>
+              মোট আদায়কৄতঃ
+            </Text>
+            <Text style={{fontSize: 35, color: 'green'}}>০.০০</Text>
+          </View>
+        </View>
+        <View>
+          <View
+            style={{
+              height: 100,
+              borderColor: 'gray',
+              width: 2,
+              borderRightColor: 'gray',
+              borderRightWidth: 2,
+              marginRight: 40,
+            }}></View>
+        </View>
+        <View>
+          <Text style={{fontSize: 20, color: 'black', fontWeight: '600'}}>
+            মোট বকেয়াকারীঃ
+          </Text>
+          <Text style={{fontSize: 35, color: 'black'}}>০</Text>
+        </View>
       </View>
 
+      {/* second section */}
+      <View
+        style={{flexDirection: 'row', justifyContent: 'center', marginTop: 20}}>
+        <View
+          style={{
+            borderLeftWidth: 1,
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            backgroundColor: '#9a9a9a',
+            width: 150,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            borderRadius: 3,
+          }}>
+          <Text style={{fontSize: 16, color: 'black', fontWeight: 'bold'}}>
+            বকেয়াকারী
+          </Text>
+        </View>
+        <View
+          style={{
+            borderRightWidth: 1,
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            backgroundColor: '#a7a7a7 ',
+            width: 150,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            borderRadius: 3,
+          }}>
+          <Text style={{fontSize: 16, color: 'black', fontWeight: 'bold'}}>
+            পরিশোধকারী
+          </Text>
+        </View>
+      </View>
+
+      {/* third section */}
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{marginLeft: 10, text: 'center'}}>
+          <Text>এই মুহুর্তে আপনার কোন বকেয়া নেই।</Text>
+          <Text>নতুন বকেয়াকারী এড করতে হলে + বাটন টি চাপুন</Text>
+          <Text>এবং প্রয়োজনীয় তথ্য পূরণ করুন</Text>
+        </View>
+      </View>
+
+      {/* last section */}
+      <View>
+        <TouchableOpacity onPress={()=> navigation.navigate("NextScreen")}>
+          <View >
+            <View style={{flexDirection:'row', justifyContent:'flex-end', marginBottom:20}}>
+              <View style={{flexDirection:'row', justifyContent:'center', alignItems:'center', width:60, height:60, borderRadius:50, backgroundColor:'#850101'}}>
+
+                <Icon name="plus"  style={{ fontSize:20,fontWeight:'100',color: 'white'}} />
+              </View>
+           </View>
+             
+            
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
-
-  </View>
-  )
+  );
 }
-
